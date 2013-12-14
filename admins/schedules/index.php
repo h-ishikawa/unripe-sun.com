73ea993e914f041d1906d3acc5f0495777d124c6
ishikawa <sevens67@i.softbank.jp>
2013-12-14 09:06:36 +0900

    スケジュール管理・スナップ管理ページ　実装

--- admins/schedules/index.php
+++ admins/schedules/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once (dirname(__FILE__).'/../../lib/Model.php');
-require_once (dirname(__FILE__).'/../../lib/Model/Tweet.php');
+require_once (dirname(__FILE__).'/../../lib/Model/Schedule.php');
 
 ?>
 
@@ -9,21 +9,41 @@ require_once (dirname(__FILE__).'/../../lib/Model/Tweet.php');
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Unripe | つぶやき管理</title>
+<title>Unripe | スケジュール管理</title>
 <meta name="robots" content="noindex,nofollow">
 <meta http-equiv="Content-Script-Type" content="text/javascript">
 <meta http-equiv="Content-Style-Type" content="text/css">
 <link type="text/css" rel="stylesheet" href="/stylesheets/common.css" media="all" charset="utf-8">
 <link type="text/css" rel="stylesheet" href="/stylesheets/admins.css" media="all" charset="utf-8">
 </head>
-<body class="admins tweets">
+<body class="admins schedules">
   <div class="header container">
     <? require_once (dirname(__FILE__).'/../../partials/admins/header.php'); ?>
   </div>
 
   <div class="contents container">
-    <form action="/lib/Model/Tweet.php" method="post">
+    <form action="/lib/Model/Schedule.php" method="post">
       <table class="new">
+        <tr>
+          <td>休日</td>
+          <td>
+            <select name="year">
+              <option value="2014">2014</option>
+              <option value="2015">2015</option>
+            </select>年
+            <select name="month">
+              <? for($i = 1; i < 13; i = i + 1): ?>
+              <option value="<?= $i ?>"><?= $i ?></option>
+              <? endfor; ?>
+            </select>月
+            <select name="day">
+              <? for($i = 1; i < 31; i = i + 1): ?>
+              <option value="<?= $i ?>"><?= $i ?></option>
+              <? endfor; ?>
+            </select>日
+          </td>
+        </tr>
+
         <tr>
           <td>スタッフ名</td>
           <td>
@@ -38,12 +58,6 @@ require_once (dirname(__FILE__).'/../../lib/Model/Tweet.php');
           </td>
         </tr>
 
-        <tr>
-          <td>つぶやき</td>
-          <td>
-            <textarea class="tweet" name="tweet" rows="6" cols="60"></textarea>
-          </td>
-        </tr>
 
         <tr>
           <td class="buttons" colspan="2">
@@ -58,8 +72,8 @@ require_once (dirname(__FILE__).'/../../lib/Model/Tweet.php');
     <table class="results">
       <tr>
         <th>ID</th>
+        <th>休日</th>
         <th>スタッフ名</th>
-        <th>つぶやき</th>
         <th>投稿日</th>
         <th></th>
       </tr>
@@ -67,11 +81,11 @@ require_once (dirname(__FILE__).'/../../lib/Model/Tweet.php');
       <? foreach ($Tweet->result as $result): ?>
       <tr>
         <td><?= $result->id ?></td>
+        <td><?= $result->date ?></td>
         <td><?= $result->stuff ?></td>
-        <td><?= $result->tweet ?></td>
         <td><?= $result->created_at ?></td>
         <td>
-          <form action="/lib/Model/Tweet.php" class="delete" method="post" onsubmit="return confirm('<?= @$result->tweet ?>を削除して宜しいですか？')">
+          <form action="/lib/Model/Tweet.php" class="delete" method="post" onsubmit="return confirm('<?= @$result->id ?>を削除して宜しいですか？')">
             <input type="hidden" name="_METHOD" value="DELETE">
             <input type="hidden" name="id" value="<?= $result->id ?>">
             <input type="submit" value="削除する">
---
+++ admins/snaps/index.php
@@ -0,0 +1,115 @@
+<?php
+
+//require_once (dirname(__FILE__).'/../../lib/Model.php');
+//require_once (dirname(__FILE__).'/../../lib/Model/Snap.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Unripe | スナップ管理</title>
+<meta name="robots" content="noindex,nofollow">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<link type="text/css" rel="stylesheet" href="/stylesheets/common.css" media="all" charset="utf-8">
+<link type="text/css" rel="stylesheet" href="/stylesheets/admins.css" media="all" charset="utf-8">
+</head>
+<body class="admins snaps">
+  <div class="header container">
+    <? require_once (dirname(__FILE__).'/../../partials/admins/header.php'); ?>
+  </div>
+
+  <div class="contents container">
+    <form action="/lib/Model/Snap.php" method="post" enctype="multipart/form-data">
+      <table class="new">
+        <tr>
+          <td>モデル名</td>
+          <td>
+              <input type="text" name="name" value="" />
+          </td>
+        </tr>
+
+        <tr>
+          <td>性別</td>
+          <td>
+              <input type="radio" name="sex" value="1" checked="checked" />女性
+              <input type="radio" name="sex" value="0" />男性
+          </td>
+        </tr>
+
+        <tr>
+          <td>画像</td>
+          <td>
+              <input type="file" name="file_path[]" multiple="multiple" />
+          </td>
+        </tr>
+
+        <tr>
+          <td>カテゴリー</td>
+          <td>
+            <select name="category">
+              <option value="0">ロング</option>
+              <option value="1">セミロング</option>
+              <option value="2">ミディアム</option>
+              <option value="3">ボブ</option>
+              <option value="4">ショート</option>
+              <option value="5">ベリーショート</option>
+              <option value="6">ヘアセット</option>
+              <option value="7">カラーリング</option>
+              <option value="8">パーマ</option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>メモ</td>
+          <td>
+            <textarea name="memo" rows="6" cols="60"></textarea>
+          </td>
+        </tr>
+
+        <tr>
+          <td class="buttons" colspan="2">
+            <input type="hidden" name="_METHOD" value="POST">
+            <input class="submit" type="submit" value="登録する">
+            <input class="reset" type="reset" value="リセット">
+          </td>
+        </tr>
+      </table>
+    </form>
+
+    <table class="results">
+      <tr>
+        <th>ID</th>
+        <th>モデル名</th>
+        <th>性別</th>
+        <th>画像</th>
+        <th>カテゴリー</th>
+        <th>メモ</th>
+        <th></th>
+      </tr>
+
+      <? foreach ($Snap->result as $result): ?>
+      <tr>
+        <td><?= $result->id ?></td>
+        <td><?= $result->name ?></td>
+        <td><?= $result->sex ?></td>
+        <td><?= $result->file_path ?></td>
+        <td><?= $result->category ?></td>
+        <td><?= $result->memo ?></td>
+        <td><?= $result->created_at ?></td>
+        <td>
+          <form action="/lib/Model/Snap.php" class="update" method="post" onsubmit="return confirm('<?= @$result->name ?>さんを削除して宜しいですか？')">
+            <input type="hidden" name="_METHOD" value="DELETE">
+            <input type="hidden" name="id" value="<?= $result->id ?>">
+            <input type="submit" value="削除する">
+          </form>
+        <td>
+      </tr>
+      <? endforeach; ?>
+    </table>
+  </div>
+</body>
+</html>
