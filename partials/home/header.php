622b46e2eca2d6c22209504575bd42b4d2cfcb2b
ishikawa <sevens67@i.softbank.jp>
2013-12-14 23:55:14 +0900

    home キービジュアル実装

--- partials/home/header.php
+++ partials/home/header.php
@@ -3,4 +3,11 @@ $uri = $_SERVER['REQUEST_URI'];
 $replaceUrl = preg_replace('/\//', ' ', $uri);
 ?>
 
-<h1 onclick="Javascript: location.href='/'">UNRIPE</h1>
+<h1 onclick="Javascript: location.href='/'">Unripe</h1>
+
+<a class="stuff tomori" href=""></a>
+<a class="stuff koike" href=""></a>
+<a class="stuff yoshino" href=""></a>
+<a class="stuff enomoto" href=""></a>
+<a class="stuff sakuma" href=""></a>
+<a class="stuff shimizu" href=""></a>
--- partials/navigation.php
+++ partials/navigation.php
@@ -3,8 +3,21 @@ $uri = $_SERVER['REQUEST_URI'];
 $replaceUrl = preg_replace('/\//', ' ', $uri);
 ?>
 
-    <a href="/"      class="panel <?= $replaceUrl == ' ' ? 'selected' : ''        ?>">home</a><!--
- --><a href="abouts" class="panle <?= $replaceUrl == ' abouts ' ? 'selected' : '' ?>">●●●</a><!--
- --><a href="">●●●</a><!--
- --><a href="">●●●</a><!--
- --><a href="">●●●</a>
+    <div class="panel links">
+      <a href="/stuffs"        class="panel <?= $replaceUrl == ' ' ? 'selected' : '' ?>              ">stuff</a><!--
+   --><a href="/menus"         class="panel <?= $replaceUrl == ' menus ' ? 'selected' : '' ?>        ">menu</a><!--
+   --><a href="/news"          class="panel <?= $replaceUrl == ' news ' ? 'selected' : '' ?>         ">news</a><!--
+   --><a href="/hair-meisters" class="panel <?= $replaceUrl == ' hair-meisters ' ? 'selected' : '' ?>">hair meister</a><!--
+   --><a href="/goods"         class="panel <?= $replaceUrl == ' goods ' ? 'selected' : '' ?>        ">goods</a><!--
+   --><a href="/galleries"     class="panel <?= $replaceUrl == ' galleries ' ? 'selected' : '' ?>    ">gallery</a><!--
+   --><a href="/accesses"      class="panel <?= $replaceUrl == ' accesses ' ? 'selected' : '' ?>     ">access</a><!--
+   --><a href="/recruits"      class="panel <?= $replaceUrl == ' recruits ' ? 'selected' : '' ?>     ">recruit</a><!--
+   --><a href="/contacts"      class="panel <?= $replaceUrl == ' contacts ' ? 'selected' : '' ?>     ">contact</a>
+    </div><!--
+
+ --><div class="panel properties">
+      <a class="panel" href="/contacts"><img src="/images/mail.png" alt=""></a><!--
+   --><a class="panel" href="http://www.facebook.com/pages/Unripe/209519589110505" target="_blank"><img src="/images/facebook.png" alt=""></a><!--
+   --><a class="panel" href="/contacts" target="_blank"><img src="/images/blogger.png" alt=""></a><!--
+   --><img class="panel cactus" src="/images/cactus.png" class="panel" alt="">
+    </div>
