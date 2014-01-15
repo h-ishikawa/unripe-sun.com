e78530d4243002e74b576f8ba4b3ad61c3d19e12
kobitoino <kobito0826@gmail.com>
2014-01-16 01:16:05 +0900

    修正前の画像追加

--- partials/news/header.php
+++ partials/news/header.php
@@ -6,7 +6,7 @@ $replaceUrl = preg_replace('/\//', ' ', $uri);
 <h1 onclick="Javascript: location.href='/'">Unripe</h1>
 
 <div class="panel"><!--
---><a class="stuff yoshino" href=""><img src="../../images/menus/yoshino.png" /></a></div><!--
+--><a class="stuff yoshino" href=""><img src="../../images/news/tomori.png" /></a></div><!--
       --><div class="panel"><h2>Unripe Menu</h2>
     				<p class="point">※是非、あなたの大切なご家族、お友達をご紹介して頂いた方には<br />
     				次回、全メニュー<span class="price-off">30%OFF</span>で施術させて頂きます。
