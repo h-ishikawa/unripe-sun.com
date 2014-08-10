b508e5ff94987b83fdc21311756c5b8c4bd76329
ishikawa <sevens67@i.softbank.jp>
2014-08-10 12:19:22 +0900

    menus 表示の変更

--- templates/menus/index.php
+++ templates/menus/index.php
@@ -9,117 +9,177 @@ $settings = (object) array(
   , 'point' => '※あなたの大切なご家族、お友達をご紹介して頂いた方には<br />次回、全メニュー<span class="price-off">30%OFF</span>で施術させて頂きます。'
 );
 ?>
- <div class="menu">
-     <h3><span></span>【カット】価格：5,400円〜 (ロング料金はかかりません。)</h3>
-     <p>カウンセリング重視♪一人ひとりの雰囲気に合わせて作るオーダーメイド感覚の似合わせスタイル♪<br><br>■店長・ディレクターカット・・・プラス￥540-<br />■オーナーカット・・・プラス￥1,080-
-       </p>
-   </div>
- 
- <div class="panel m-detail">
-     <p><img src="/public/images/menus/trend01.jpg">
-     </p>
-   </div><!--
+<style>
+table{
+  width:100%;
+  border-collapse:collapse;
+  margin:0 0 30px 0;
+}
+th{
+  color:#fff;
+  background-color:#330000;
+}
+td{
+  text-align:right;
+  border-bottom:dotted 1px #7c6551;
+  font-size:90%;
+}
+td.title{
+  text-align:left;
+  font-weight:bold;
+  //color:#ff6347;
+}
+td.point{
+  text-align:left;
+  border-bottom:dotted 1px #7c6551;
+  padding:0 5px 8px;
+}
+th,td{
+  padding:8px 5px;
+}
+.mn th,.mn td{
+  border:none;
+}
+</style>
 
---><div class="panel m-detail">
-     <p><img src="/public/images/menus/trend03.jpg">
-       </p>
-   </div><!--
+<table>
+  <tr>
+    <th colspan="2"><h3>カット</h3></th>
+  <tr>
+  <tr class="mn">
+    <td class="title">基本料金(ロング料金はかかりません。)</td>
+    <td>￥5,400〜</td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">カウンセリング重視♪一人ひとりの雰囲気に合わせて作るオーダーメイド感覚の似合わせスタイル♪</td>
+  <tr>
+  <tr>
+    <td class="title">店長・ディレクターカット</td>
+    <td>プラス￥540</td>
+  <tr>
+  <tr>
+    <td class="title">オーナーカット</td>
+    <td>プラス￥1,080</td>
+  <tr>
+</table>
 
---><div class="panel m-detail">
-     <p><img src="/public/images/menus/trend02.jpg">
-       </p>
-   </div><!--
+<table>
+  <tr>
+    <th colspan="2"><h3>トリートメント</h3></th>
+  <tr>
+  <tr class="mn">
+    <td class="title">基本料金</td>
+    <td>￥3,780〜</td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">あなただけのオーダーメイドトリートメント★Aujuaなら髪の悩みを改善出来る!!<br>今までの髪の悩みも解消出来る!!実力派のスタイリストに相談してください。</td>
+  <tr>
+  <tr class="mn">
+    <td class="title">オージェアトリートメント</td>
+    <td></td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">限られた店舗でしか置くことが出来ない、Aujuaのトリートメントは5184通りの中からあなただけに合ったプログラムを作成</td>
+  <tr>
+  <tr class="mn">
+    <td class="title">ヘッドスパ</td>
+    <td></td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">頭皮の汚れを取り除き、内側からキレイに♪リフトアップ効果も期待できる、頭皮のエステ！</td>
+  <tr>
+  <tr class="mn">
+    <td class="title">水素ホイップトリートメント</td>
+    <td></td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">パーマやカラーのメニューと組み合わせることによって、今までにないツヤと手触り！<br>凝縮した生ケラチン・生コラーゲンの力でグングン浸透。カラーの色持ちや、パーマのカールの持ちが良くなります。</td>
+  <tr>
+</table>
 
---><div class="panel m-detail">
-     <p><img src="/public/images/menus/trend06.jpg">
-       </p>
-   </div>
-   
- <div class="menu">
-     <h3><span></span>【トリートメント】価格：3,780円〜</h3>
-     <p>あなただけのオーダーメイドトリートメント★Aujuaなら髪の悩みを改善出来る!!
-       </p>
-   </div>
- 
- <div class="panel m-detail">
-     <h3><span></span>トリートメント</h3>
-     <p><img src="/public/images/goods/powerdict.png">今までの髪の悩みも解消出来る!!実力派のスタイリストに相談してください。
-       </p>
-   </div><!--
+<table>
+  <tr>
+    <th colspan="2"><h3>カット&カラー</h3></th>
+  <tr>
+  <tr class="mn">
+    <td class="title">基本料金</td>
+    <td>￥12,420〜</td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">素髪を傷ませずに色を入れるから、いつまでもなめらかで透明感のある艶カラー♪</td>
+  <tr>
+  <tr class="mn">
+    <td class="title">カラーリング</td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">600種類以上のカラー剤の中から、お客様1人1人に合うオンリーワンカラーを選びます。<br>スタイリストは肌色診断も出来る為、お客様に一番合う色を提案させて頂きます。</td>
+  <tr>
+</table>
 
---><div class="panel m-detail">
-     <h3><span></span>オージェアトリートメント</h3>
-     <p><img src="/public/images/goods/aujua_tr.jpg">限られた店舗でしか置くことが出来ない、Aujuaのトリートメントは5184通りの中からあなただけに合ったプログラムを作成
-       </p>
-   </div><!--
+<table>
+  <tr>
+    <th colspan="2"><h3>カット&パーマ</h3></th>
+  <tr>
+  <tr class="mn">
+    <td class="title">基本料金</td>
+    <td>￥12,960〜</td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">髪質にあわせて、15種類の薬剤の中から選びます。トリートメントをしたような手触りに仕上がります！</td>
+  <tr>
+  <tr>
+    <td class="title">カット&デジタルパーマ</td>
+    <td>￥18,360〜</td>
+  <tr>
+  <tr>
+    <td class="title">カット&ストレートパーマ</td>
+    <td>￥18,360〜</td>
+  <tr>
+  <tr class="mn">
+    <td class="title">パーマ</td>
+    <td></td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">髪質にあわせて、15種類の薬剤の中から選びます。トリートメントをしたような手触りに仕上がります！</td>
+  <tr>
+  <tr class="mn">
+    <td class="title">デジタルパーマ</td>
+    <td></td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">従来のデジタルパーマより、低温でかける為ダメージが少なく、潤艶な質感と手触りが出せます。<br>パーマ液は酸性～化粧品まであり、なりたいイメージに合わせて使用します。</td>
+  <tr>
+</table>
 
---><div class="panel m-detail">
-     <h3><span></span>ヘッドスパ</h3>
-     <p><img src="/public/images/menus/spa.jpg">頭皮の汚れを取り除き、内側からキレイに♪リフトアップ効果も期待できる、頭皮のエステ！
-       </p>
-   </div><!--
+<table>
+  <tr>
+    <th colspan="2"><h3>スタイリング</h3></th>
+  <tr>
+  <tr class="mn">
+    <td class="title">基本料金</td>
+    <td>お問い合わせください</td>
+  <tr>
+  <tr>
+    <td class="point" colspan="2">お気軽にスタイリングも致します。</td>
+  <tr>
+</table>
 
---><div class="panel m-detail">
-     <h3><span></span>水素ホイップトリートメント</h3>
-     <p><img src="/public/images/menus/whip.jpg">パーマやカラーのメニューと組み合わせることによって、今までにないツヤと手触り！<br>凝縮した生ケラチン・生コラーゲンの力でグングン浸透。カラーの色持ちや、パーマのカールの持ちが良くなります。
-       </p>
-   </div>  
+<table>
+  <tr>
+    <th colspan="2"><h3>セットアップ</h3></th>
+  <tr>
+  <tr>
+    <td class="title">基本料金</td>
+    <td>￥6,480</td>
+  <tr>
+</table>
 
- <div class="menu">
-     <h3><span></span>【カット&カラー】価格：12,420円〜</h3>
-     <p>素髪を傷ませずに色を入れるから、いつまでもなめらかで透明感のある艶カラー♪
-       </p>
-   </div>
- 
- <div class="panel m-detail">
-     <h3><span></span>カラーリング</h3>
-     <p><img src="/public/images/goods/color.jpg">600種類以上のカラー剤の中から、お客様1人1人に合うオンリーワンカラーを選びます。スタイリストは肌色診断も出来る為、お客様に一番合う色を提案させて頂きます。
-     </p>
-   </div>
-      
-<div class="menu">
-     <h3><span></span>【カット&amp;パーマ】価格：12,960円〜</h3>
-     <p>髪質にあわせて、15種類の薬剤の中から選びます。トリートメントをしたような手触りに仕上がります！
-       </p>
-   </div>
- 
- <div class="panel m-detail">
-     <h3><span></span>パーマ</h3>
-     <p><img src="/public/images/menus/parm.jpg">髪質にあわせて、15種類の薬剤の中から選びます。トリートメントをしたような手触りに仕上がります！
-       </p>
-   </div><!--
-
---><div class="panel m-detail">
-     <h3><span></span>デジタルパーマ</h3>
-     <p><img src="/public/images/menus/digitalparm.jpg">従来のデジタルパーマより、<br>低温でかける為ダメージが少なく、潤艶な質感と手触りが出せます。パーマ液は酸性～化粧品まであり、なりたいイメージに合わせて使用します。
-       </p>
-   </div>
-
-<div class="menu">
-     <h3><span></span>【スタイリング】</h3>
-     <p>お気軽にスタイリングも致します。
-       </p>
-   </div>
-   
-<div class="menu">
-     <h3><span></span>カット＆デジタルパーマ</h3>
-     <p>価格：18,360円〜
-       </p>
-   </div>
-<div class="menu">
-     <h3><span></span>カット＆ストレートパーマ</h3>
-     <p>価格：18,360円〜
-       </p>
-   </div>
- 
- <div class="panel m-detail">
-     <h3><span></span>セットアップ</h3>
-     <p><img src="/public/images/menus/setup.jpg">価格：6,480円
-       </p>
-   </div><!--
-
---><div class="panel m-detail">
-     <h3><span></span>前髪カット</h3>
-     <p><img src="/public/images/menus/maegami.jpg">価格：1,080円
-       </p>
-   </div>
+<table>
+  <tr>
+    <th colspan="2"><h3>前髪カット</h3></th>
+  <tr>
+  <tr>
+    <td class="title">基本料金</td>
+    <td>￥1,080</td>
+  <tr>
+</table>
