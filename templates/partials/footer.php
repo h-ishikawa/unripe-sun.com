56856a885c233710b4e1d253b05328a48d5fdc88
kobitoino <kobito0826@gmail.com>
2015-10-17 22:58:21 +0900

    footer修正

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,2 +1,2 @@
 <link type="text/css" rel="stylesheet" href="/stylesheets/common.css" media="all" charset="utf-8">
-<div class="copyright"><img src="/public/images/home/footer_bg.png"><br>Copyright &copy; 2014 <strong>unripe</strong> All Rights Reserved.</div>
+<div class="copyright"><img src="/public/images/home/footer_bg.png"><br>Copyright &copy; 2015 <strong>unripe</strong> All Rights Reserved.</div>
